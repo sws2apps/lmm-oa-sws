92b524cd0ebfdec9fbebeea5c88542594a24c0b7
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-03-05 20:26:36 +0300

    fix(updater): localize update notification

--- src/components/root/ServiceWorkerWrapper.js
+++ src/components/root/ServiceWorkerWrapper.js
@@ -1,68 +1,70 @@
 import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
 import * as serviceWorkerRegistration from '../../serviceWorkerRegistration';
 
 const ServiceWorkerWrapper = (props) => {
-  const { updatePwa } = props;
-  const [showReload, setShowReload] = useState(false);
-  const [isPrecached, setIsPrecached] = useState(false);
-  const [waitingWorker, setWaitingWorker] = useState(null);
+	const { updatePwa } = props;
+	const { t } = useTranslation();
 
-  const onSWUpdate = (registration) => {
-    setShowReload(true);
-    setWaitingWorker(registration.waiting);
-  };
+	const [showReload, setShowReload] = useState(false);
+	const [isPrecached, setIsPrecached] = useState(false);
+	const [waitingWorker, setWaitingWorker] = useState(null);
 
-  const onSWInstalled = () => {
-    setIsPrecached(true);
-  };
+	const onSWUpdate = (registration) => {
+		setShowReload(true);
+		setWaitingWorker(registration.waiting);
+	};
 
-  useEffect(() => {
-    serviceWorkerRegistration.register({ onUpdate: onSWUpdate, onSuccess: onSWInstalled });
-  });
+	const onSWInstalled = () => {
+		setIsPrecached(true);
+	};
 
-  const reloadPage = () => {
-    waitingWorker?.postMessage({ type: 'SKIP_WAITING' });
-    updatePwa();
-    setShowReload(false);
-    window.location.reload();
-  };
+	useEffect(() => {
+		serviceWorkerRegistration.register({
+			onUpdate: onSWUpdate,
+			onSuccess: onSWInstalled,
+		});
+	});
 
-  return (
-    <>
-      <Snackbar
-        open={showReload}
-        message="Misy version vaovao ny LMM-OA --->"
-        onClick={reloadPage}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-        action={
-          <Button
-            color="inherit"
-            size="small"
-            onClick={reloadPage}
-          >
-            Hampiditra
-          </Button>
-        }
-      />
-      <Snackbar
-        open={isPrecached}
-        message="Afaka mampiasa ny LMM-OA ianao izao na tsy misy internet aza."
-        onClick={() => setIsPrecached(false)}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-        action={
-          <Button
-            color="inherit"
-            size="small"
-            onClick={() => setIsPrecached(false)}
-          >
-            OK
-          </Button>
-        }
-      />
-    </>
-  );
-}
+	const reloadPage = () => {
+		waitingWorker?.postMessage({ type: 'SKIP_WAITING' });
+		updatePwa();
+		setShowReload(false);
+		window.location.reload();
+	};
+
+	return (
+		<>
+			<Snackbar
+				open={showReload}
+				message={t('global.newVersion')}
+				onClick={reloadPage}
+				anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+				action={
+					<Button color='inherit' size='small' onClick={reloadPage}>
+						{t('global.updateApp')}
+					</Button>
+				}
+			/>
+			<Snackbar
+				open={isPrecached}
+				message={t('global.cacheCompleted')}
+				onClick={() => setIsPrecached(false)}
+				anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+				action={
+					<Button
+						color='inherit'
+						size='small'
+						onClick={() => setIsPrecached(false)}
+					>
+						OK
+					</Button>
+				}
+			/>
+		</>
+	);
+};
 
 export default ServiceWorkerWrapper;
