de413c796ee178a88396adaf93e79e3dd2ea722c
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-04-30 18:28:54 +0300

    feat(app): register service worker on initial page load

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { RecoilRoot } from 'recoil';
 import RecoilOutside from 'recoil-outside';
 import './i18n';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -19,3 +20,5 @@ root.render(
 		<App />
 	</RecoilRoot>
 );
+
+serviceWorkerRegistration.register();
