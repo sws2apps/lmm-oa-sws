2e06da113f1569dc96a33aaf5e4f9dd31fe8b5fc
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-05-14 16:49:29 +0300

    fix(sw): update exp 3

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -18,7 +18,8 @@ export function register(config) {
 			// serve assets; see https://github.com/facebook/create-react-app/issues/2374
 			return;
 		}
-
+		
+		console.log('register-before-evl');
 		window.addEventListener('load', () => {
 			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 			console.log('event-listener-run');
