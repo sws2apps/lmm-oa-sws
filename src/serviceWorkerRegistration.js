f8fdfad6015fb7ab19398fba515b05123ede573c
rhahao <rhahao.vj@gmail.com>
2022-05-14 21:49:43 +0300

    restore sw

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,3 +1,15 @@
+// This optional code is used to register a service worker.
+// register() is not called by default.
+
+// This lets the app load faster on subsequent visits in production, and gives
+// it offline capabilities. However, it also means that developers (and users)
+// will only see deployed updates on subsequent visits to a page, after all the
+// existing tabs open on the page have been closed, since previously cached
+// resources are updated in the background.
+
+// To learn more about the benefits of this model and instructions on how to
+// opt-in, read https://cra.link/PWA
+
 const isLocalhost = Boolean(
 	window.location.hostname === 'localhost' ||
 		// [::1] is the IPv6 localhost address.
@@ -21,6 +33,7 @@ export function register(config) {
 
 		window.addEventListener('load', () => {
 			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
 			if (isLocalhost) {
 				// This is running on localhost. Let's check if a service worker still exists or not.
 				checkValidServiceWorker(swUrl, config);
@@ -46,42 +59,38 @@ function registerValidSW(swUrl, config) {
 		.register(swUrl)
 		.then((registration) => {
 			registration.onupdatefound = () => {
-				console.log(registration);
 				const installingWorker = registration.installing;
-				
-				if (installingWorker) {
-					installingWorker.onstatechange = () => {
-						console.log(installingWorker.state);
-						if (installingWorker.state === 'installed') {
-							console.log(registration);
-							const waitingWorker = registration.waiting;
-							if (waitingWorker) {
-								// At this point, the updated precached content has been fetched,
-								// but the previous service worker will still serve the older
-								// content until all client tabs are closed.
-								console.log(
-									'New content is available and will be used when all ' +
-										'tabs for this page are closed. See https://cra.link/PWA.'
-								);
+				if (installingWorker == null) {
+					return;
+				}
+				installingWorker.onstatechange = () => {
+					if (installingWorker.state === 'installed') {
+						if (navigator.serviceWorker.controller) {
+							// At this point, the updated precached content has been fetched,
+							// but the previous service worker will still serve the older
+							// content until all client tabs are closed.
+							console.log(
+								'New content is available and will be used when all ' +
+									'tabs for this page are closed. See https://cra.link/PWA.'
+							);
 
-								// Execute callback
-								if (config && config.onUpdate) {
-									config.onUpdate(registration);
-								}
-							} else {
-								// At this point, everything has been precached.
-								// It's the perfect time to display a
-								// "Content is cached for offline use." message.
-								console.log('Content is cached for offline use.');
+							// Execute callback
+							if (config && config.onUpdate) {
+								config.onUpdate(registration);
+							}
+						} else {
+							// At this point, everything has been precached.
+							// It's the perfect time to display a
+							// "Content is cached for offline use." message.
+							console.log('Content is cached for offline use.');
 
-								// Execute callback
-								if (config && config.onSuccess) {
-									config.onSuccess();
-								}
+							// Execute callback
+							if (config && config.onSuccess) {
+								config.onSuccess(registration);
 							}
 						}
-					};
-				}
+					}
+				};
 			};
 		})
 		.catch((error) => {
