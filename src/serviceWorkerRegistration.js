e2ed24952c91e7912c38163de4a0e55097d2c649
rhahao <rhahao.vj@gmail.com>
2022-05-14 17:28:36 +0300

    fix sw update

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -18,7 +18,7 @@ export function register(config) {
 			// serve assets; see https://github.com/facebook/create-react-app/issues/2374
 			return;
 		}
-		
+
 		window.addEventListener('load', () => {
 			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 			if (isLocalhost) {
@@ -47,40 +47,37 @@ function registerValidSW(swUrl, config) {
 		.then((registration) => {
 			registration.onupdatefound = () => {
 				const installingWorker = registration.installing;
-				
 				const waitingWorker = registration.waiting;
-				console.log(waitingWorker);
-				
+
 				if (installingWorker) {
 					installingWorker.onstatechange = () => {
-					console.log(installingWorker.state);
-					if (installingWorker.state === 'installed') {
-						if (waitingWorker) {
-							// At this point, the updated precached content has been fetched,
-							// but the previous service worker will still serve the older
-							// content until all client tabs are closed.
-							console.log(
-								'New content is available and will be used when all ' +
-									'tabs for this page are closed. See https://cra.link/PWA.'
-							);
+						if (installingWorker.state === 'installed') {
+							if (waitingWorker) {
+								// At this point, the updated precached content has been fetched,
+								// but the previous service worker will still serve the older
+								// content until all client tabs are closed.
+								console.log(
+									'New content is available and will be used when all ' +
+										'tabs for this page are closed. See https://cra.link/PWA.'
+								);
 
-							// Execute callback
-							if (config && config.onUpdate) {
-								config.onUpdate(registration);
-							}
-						} else {
-							// At this point, everything has been precached.
-							// It's the perfect time to display a
-							// "Content is cached for offline use." message.
-							console.log('Content is cached for offline use.');
+								// Execute callback
+								if (config && config.onUpdate) {
+									config.onUpdate(registration);
+								}
+							} else {
+								// At this point, everything has been precached.
+								// It's the perfect time to display a
+								// "Content is cached for offline use." message.
+								console.log('Content is cached for offline use.');
 
-							// Execute callback
-							if (config && config.onSuccess) {
-								config.onSuccess();
+								// Execute callback
+								if (config && config.onSuccess) {
+									config.onSuccess();
+								}
 							}
 						}
-					}
-				};
+					};
 				}
 			};
 		})
