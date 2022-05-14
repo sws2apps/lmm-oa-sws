7ec60c8fe724a28d2dd1eeed2071f8caaca35b01
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-05-14 16:43:25 +0300

    fix(sw): update exp 2

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -21,7 +21,7 @@ export function register(config) {
 
 		window.addEventListener('load', () => {
 			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
+			console.log('event-listener-run');
 			if (isLocalhost) {
 				// This is running on localhost. Let's check if a service worker still exists or not.
 				checkValidServiceWorker(swUrl, config);
@@ -46,6 +46,7 @@ function registerValidSW(swUrl, config) {
 	navigator.serviceWorker
 		.register(swUrl)
 		.then((registration) => {
+			console.log(registration);
 			registration.onupdatefound = () => {
 				const installingWorker = registration.installing;
 
