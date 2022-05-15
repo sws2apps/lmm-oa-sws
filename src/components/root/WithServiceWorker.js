553196e5baa10f7c2691a5bd1b69e58b5c499e8b
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-05-15 22:30:25 +0300

    fix(sw): update exp 20

--- src/components/root/WithServiceWorker.js
+++ src/components/root/WithServiceWorker.js
@@ -118,6 +118,7 @@ export default class WithServiceWorker extends Component {
 				const installingWorker = registration.installing;
 				installingWorker.onstatechange = () => {
 					if (installingWorker.state === 'installed') {
+						console.log(navigator.serviceWorker)
 						if (navigator.serviceWorker.controller) {
 							onUpdated && onUpdated();
 						}
