55c64e6a2e9629dd7c9d9f95bbf527fc5d238f1d
rhahao <rhahao.vj@gmail.com>
2022-05-16 18:23:25 +0300

    update sw hook to functional component

--- src/components/root/WithServiceWorker.js
+++ src/components/root/WithServiceWorker.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const isLocalhost = Boolean(
@@ -12,95 +12,19 @@ const isLocalhost = Boolean(
 			))
 );
 
-export default class WithServiceWorker extends Component {
-	static propTypes = {
-		children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]).isRequired,
-		onError: PropTypes.func,
-		onInstalled: PropTypes.func,
-		onUpdated: PropTypes.func,
-		onStaled: PropTypes.func,
-		publicServiceWorkerDest: PropTypes.string.isRequired,
-		publicUrl: PropTypes.string,
-		registerOnMount: PropTypes.bool,
-	};
+const ServiceWorkerWrapper = ({
+	children,
+	onError,
+	onInstalled,
+	onUpdated,
+	onStaled,
+	publicServiceWorkerDest,
+	publicUrl,
+}) => {
+	const [registration, setRegistration] = useState(null);
+	const [serviceWorkerUrl] = useState(`${publicUrl}${publicServiceWorkerDest}`);
 
-	static defaultProps = {
-		onError: null,
-		onInstalled: null,
-		onUpdated: null,
-		onStaled: null,
-		publicUrl: '',
-		registerOnMount: true,
-	};
-
-	state = {
-		registration: null,
-		serviceWorkerUrl: `${this.props.publicUrl}${this.props.publicServiceWorkerDest}`,
-	};
-
-	componentDidMount = () => {
-		const { registerOnMount } = this.props;
-		if (registerOnMount) {
-			this.register();
-		}
-	};
-
-	register = () => {
-		if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-			// The URL constructor is available in all browsers that support SW.
-			const publicUrl = new URL(this.props.publicUrl, window.location);
-			if (publicUrl.origin !== window.location.origin) {
-				// Our service worker won't work if PUBLIC_URL is on a different origin
-				// from what our page is served on. This might happen if a CDN is used to
-				// serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
-				return;
-			}
-			if (isLocalhost) {
-				// This is running on localhost. Lets check if a service worker still exists or not.
-				this.checkValidServiceWorker();
-				// Add some additional logging to localhost, pointing developers to the
-				// service worker/PWA documentation.
-				navigator.serviceWorker.ready.then(() => {
-					console.log(
-						'This web app is being served cache-first by a service ' +
-							'worker. To learn more, visit https://goo.gl/SC7cgQ'
-					);
-				});
-			} else {
-				// Is not local host. Just register service worker
-				this.registerValidServiceWorker();
-			}
-		}
-	};
-
-	checkValidServiceWorker = async () => {
-		const { serviceWorkerUrl } = this.state;
-		try {
-			const response = await fetch(serviceWorkerUrl);
-			// Ensure service worker exists, and that we really are getting a JS file.
-			if (
-				response.status === 404 ||
-				response.headers.get('content-type').indexOf('javascript') === -1
-			) {
-				// No service worker found. Probably a different app. Reload the page.
-				navigator.serviceWorker.ready.then(async (registration) => {
-					await registration.unregister();
-					window.location.reload();
-				});
-			} else {
-				// Service worker found. Proceed as normal.
-				this.registerValidServiceWorker();
-			}
-		} catch (err) {
-			console.log(
-				'No internet connection found. App is running in offline mode.'
-			);
-		}
-	};
-
-	registerValidServiceWorker = async () => {
-		const { onError, onInstalled, onUpdated, onStaled } = this.props;
-		const { serviceWorkerUrl } = this.state;
+	const registerValidServiceWorker = useCallback(async () => {
 		try {
 			let registration = await navigator.serviceWorker.getRegistration();
 			if (!registration) {
@@ -113,7 +37,7 @@ export default class WithServiceWorker extends Component {
 				onStaled && onStaled();
 			}
 
-			this.setState({ registration });
+			setRegistration(registration);
 
 			registration.addEventListener('updatefound', () => {
 				const installingWorker = registration.installing;
@@ -145,29 +69,101 @@ export default class WithServiceWorker extends Component {
 			console.error('Error during service worker registration:', err);
 			onError && onError(err);
 		}
-	};
+	}, [serviceWorkerUrl, onError, onStaled, onInstalled, onUpdated]);
 
-	unregister = () => {
-		const { registration } = this.state;
+	const checkValidServiceWorker = useCallback(async () => {
+		try {
+			const response = await fetch(serviceWorkerUrl);
+			// Ensure service worker exists, and that we really are getting a JS file.
+			if (
+				response.status === 404 ||
+				response.headers.get('content-type').indexOf('javascript') === -1
+			) {
+				// No service worker found. Probably a different app. Reload the page.
+				navigator.serviceWorker.ready.then(async (registration) => {
+					await registration.unregister();
+					window.location.reload();
+				});
+			} else {
+				// Service worker found. Proceed as normal.
+				registerValidServiceWorker();
+			}
+		} catch (err) {
+			console.log(
+				'No internet connection found. App is running in offline mode.'
+			);
+		}
+	}, [registerValidServiceWorker, serviceWorkerUrl]);
+
+	const register = useCallback(async () => {
+		if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+			// The URL constructor is available in all browsers that support SW.
+			const pUrl = new URL(publicUrl, window.location);
+			if (pUrl.origin !== window.location.origin) {
+				// Our service worker won't work if PUBLIC_URL is on a different origin
+				// from what our page is served on. This might happen if a CDN is used to
+				// serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
+				return;
+			}
+			if (isLocalhost) {
+				// This is running on localhost. Lets check if a service worker still exists or not.
+				checkValidServiceWorker();
+				// Add some additional logging to localhost, pointing developers to the
+				// service worker/PWA documentation.
+				navigator.serviceWorker.ready.then(() => {
+					console.log(
+						'This web app is being served cache-first by a service ' +
+							'worker. To learn more, visit https://goo.gl/SC7cgQ'
+					);
+				});
+			} else {
+				// Is not local host. Just register service worker
+				registerValidServiceWorker();
+			}
+		}
+	}, [checkValidServiceWorker, publicUrl, registerValidServiceWorker]);
+
+	const unregister = useCallback(() => {
 		registration && registration.unregister();
-	};
+	}, [registration]);
 
-	update = () => {
-		const { registration } = this.state;
+	const update = useCallback(() => {
 		registration && registration.update();
 		registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-	};
+	}, [registration]);
 
-	render = () => {
-		const { children } = this.props;
-		const { registration } = this.state;
-		return typeof children === 'function'
-			? children({
-					registration,
-					register: this.register,
-					update: this.update,
-					unregister: this.unregister,
-			  })
-			: children;
-	};
-}
+	useEffect(() => {
+		if (process.env.NODE_ENV === 'production') {
+			register();
+		}
+	}, [register]);
+
+	return typeof children === 'function'
+		? children({
+				registration,
+				register: register,
+				update: update,
+				unregister: unregister,
+		  })
+		: children;
+};
+
+ServiceWorkerWrapper.propTypes = {
+	children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]).isRequired,
+	onError: PropTypes.func,
+	onInstalled: PropTypes.func,
+	onUpdated: PropTypes.func,
+	onStaled: PropTypes.func,
+	publicServiceWorkerDest: PropTypes.string.isRequired,
+	publicUrl: PropTypes.string,
+};
+
+ServiceWorkerWrapper.defaultProps = {
+	onError: null,
+	onInstalled: null,
+	onUpdated: null,
+	onStaled: null,
+	publicUrl: '',
+};
+
+export default ServiceWorkerWrapper;
