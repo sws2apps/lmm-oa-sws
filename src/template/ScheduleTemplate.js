5a3fdd27e71106241946d4f20bb3c12e9f67c9f8
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-05-17 20:38:52 +0300

    fix(schedule-template): html render exp 2

--- src/template/ScheduleTemplate.js
+++ src/template/ScheduleTemplate.js
@@ -1,12 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import { useTranslation } from 'react-i18next';
-import html2pdf from 'html2pdf.js';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import SaveAltIcon from '@mui/icons-material/SaveAlt';
-import Typography from '@mui/material/Typography';
 import { dbGetScheduleForPrint } from '../indexedDb/dbAssignment';
 import {
 	classCountState,
@@ -18,7 +12,6 @@ import { monthNamesState } from '../appStates/appSettings';
 
 const ScheduleTemplate = () => {
 	let navigate = useNavigate();
-	const { t } = useTranslation();
 
 	const [data, setData] = useState([]);
 	const [month, setMonth] = useState('');
@@ -29,17 +22,7 @@ const ScheduleTemplate = () => {
 	const congNumber = useRecoilValue(congNumberState);
 	const monthNames = useRecoilValue(monthNamesState);
 
-	const savePDF = () => {
-		const element = document.getElementById('schedule_template');
-		var opt = {
-			margin: [0.2, 0.5, 0.2, 0.5],
-			filename: `${currentSchedule.replace('/', '-')}.pdf`,
-			image: { type: 'jpeg', quality: 0.98 },
-			html2canvas: { scale: 2 },
-			jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' },
-		};
-		html2pdf().set(opt).from(element).save();
-	};
+	console.log(data, month, classCount, congName, congNumber)
 
 	useEffect(() => {
 		const getData = async () => {
