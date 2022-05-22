c798a9565e0103ae736ed6b62a529cb218fd9784
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-05-22 13:01:13 +0300

    fix(email-utils): validate email address with underscore

--- src/utils/emailValid.js
+++ src/utils/emailValid.js
@@ -2,7 +2,7 @@
 
 export const isEmailValid = (email) => {
 	const emailRegex =
-		/^([A-Za-z\d\.-]+)@([A-Za-z\d-]+)\.([A-Za-z]{2,6})(\.[A-Za-z]{2,6})?$/;
+		/^([A-Za-z\d\.\-\_]+)@([A-Za-z\d-]+)\.([A-Za-z]{2,6})(\.[A-Za-z]{2,6})?$/;
 
 	return emailRegex.test(email);
 };
