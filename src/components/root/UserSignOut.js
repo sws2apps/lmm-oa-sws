cace0bef709fbfb37f50fc7c7c77715cbc45fc5e
rhahao <rhahao.vj@gmail.com>
2022-06-25 16:19:30 +0300

    handle logout when online

--- src/components/root/UserSignOut.js
+++ src/components/root/UserSignOut.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import Box from '@mui/material/Box';
@@ -9,32 +9,46 @@ import DialogTitle from '@mui/material/DialogTitle';
 
 // states
 import {
+	apiHostState,
 	isAppClosingState,
 	isAppLoadState,
+	isOnlineState,
 	isSetupState,
 	isUserMfaSetupState,
 	isUserMfaVerifyState,
 	isUserSignInState,
 	userEmailState,
-	userPasswordState,
+	visitorIDState,
 } from '../../appStates/appSettings';
+import {
+	congAccountConnectedState,
+	offlineOverrideState,
+} from '../../appStates/appCongregation';
 
 // utils
 import { dbUpdateAppSettings } from '../../indexedDb/dbAppSettings';
 
 const UserSignOut = () => {
+	let abortCont = useMemo(() => new AbortController(), []);
+
 	const { t } = useTranslation();
 
 	const [open, setOpen] = useRecoilState(isAppClosingState);
+	const [userEmail, setUserEmail] = useRecoilState(userEmailState);
+	const [congAccountConnected, setCongAccountConnectedState] = useRecoilState(
+		congAccountConnectedState
+	);
 
 	const setIsAppLoad = useSetRecoilState(isAppLoadState);
 	const setIsSetup = useSetRecoilState(isSetupState);
 	const setUserMfaSetup = useSetRecoilState(isUserMfaSetupState);
 	const setUserMfaVerify = useSetRecoilState(isUserMfaVerifyState);
 	const setUserSignIn = useSetRecoilState(isUserSignInState);
-	const setUserEmail = useSetRecoilState(userEmailState);
+	const setOfflineOverrideState = useSetRecoilState(offlineOverrideState);
 
-	const userPwd = useRecoilValue(userPasswordState);
+	const isOnline = useRecoilValue(isOnlineState);
+	const apiHost = useRecoilValue(apiHostState);
+	const visitorID = useRecoilValue(visitorIDState);
 
 	const handleClose = (event, reason) => {
 		if (reason === 'clickaway' || reason === 'backdropClick') {
@@ -43,31 +57,59 @@ const UserSignOut = () => {
 		setOpen(false);
 	};
 
-	useEffect(() => {
-		const handleLoggout = async () => {
-			await dbUpdateAppSettings({ isLoggedOut: true });
+	const handleLogout = useCallback(async () => {
+		if (isOnline && congAccountConnected) {
+			if (apiHost !== '') {
+				await fetch(`${apiHost}api/users/logout`, {
+					signal: abortCont.signal,
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+						visitor_id: visitorID,
+						email: userEmail,
+					},
+				});
 
-			setOpen(false);
-			setIsAppLoad(true);
-			setIsSetup(true);
-			setUserMfaSetup(false);
-			setUserMfaVerify(false);
-			setUserSignIn(true);
-			setUserEmail('');
-		};
+				setCongAccountConnectedState(false);
+			}
+		}
 
-		if (open) handleLoggout();
+		localStorage.removeItem('email');
+		await dbUpdateAppSettings({ isLoggedOut: true });
+
+		setOpen(false);
+		setOfflineOverrideState(false);
+		setIsAppLoad(true);
+		setIsSetup(true);
+		setUserMfaSetup(false);
+		setUserMfaVerify(false);
+		setUserSignIn(true);
+		setUserEmail('');
 	}, [
-		open,
+		abortCont,
+		apiHost,
+		congAccountConnected,
+		isOnline,
+		setCongAccountConnectedState,
 		setIsAppLoad,
 		setIsSetup,
+		setOfflineOverrideState,
 		setOpen,
 		setUserMfaSetup,
 		setUserMfaVerify,
 		setUserSignIn,
 		setUserEmail,
-		userPwd,
+		userEmail,
+		visitorID,
 	]);
+
+	useEffect(() => {
+		if (open) handleLogout();
+	}, [handleLogout, open]);
+
+	useEffect(() => {
+		return () => abortCont.abort();
+	}, [abortCont]);
 
 	return (
 		<Box>
