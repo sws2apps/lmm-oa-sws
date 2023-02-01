b5637a4f4c6d3e157c899f7210d63116711a7f93
rhahao <26148770+rhahao@users.noreply.github.com>
2023-02-01 13:06:33 +0300

    fix bug when convention week

--- src/i18n.js
+++ src/i18n.js
@@ -7,15 +7,9 @@ const resources = {};
 // programatically load all locales
 for await (const language of LANGUAGE_LIST) {
   // load ui namespace
-<<<<<<< HEAD
-  const ui = await import(`./locales/${language.code}/ui.json`).then((module) => module.default);
-  // load source namespace
-  const source = await import(`./locales/${language.code}/source.json`).then((module) => module.default);
-=======
   const ui = await import(`./locales/${language.locale}/ui.json`).then((module) => module.default);
   // load source namespace
   const source = await import(`./locales/${language.locale}/source.json`).then((module) => module.default);
->>>>>>> 6e3b1db33e16ba9f987241c3effad690d52a3341
 
   resources[language.code] = {
     ui,
